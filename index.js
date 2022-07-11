693fee4285c03bfbea7d5819e21e5719e9573903
Pedro Colle <pedro.h.b.colle@gmail.com>
2022-07-11 14:32:58 -0300

    feito o js do menu + o menu e a parte de estrutura do css

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function menuToggle(subMenuId,itemListId){
    
-	document.querySelectorAll('.MenuButtons').forEach(e=>e.style.backgroundColor = 'blue')
+	document.querySelectorAll('.MenuButtons').forEach(e=>e.style.backgroundColor = 'black')
 	document.querySelectorAll('.MenuItems').forEach(e=>e.style.display = 'none')
 
-	document.getElementById(subMenuId).style.backgroundColor = 'red'
+	document.getElementById(subMenuId).style.backgroundColor = 'red' 
 	document.getElementById(itemListId).style.display = 'block'
 }
