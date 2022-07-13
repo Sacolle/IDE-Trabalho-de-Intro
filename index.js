3e4b05bf0ffbed39e1053f93acc4f40f1aed0e88
Pedro Colle <pedro.h.b.colle@gmail.com>
2022-07-13 18:23:52 -0300

    finalizada a parte do Menu

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const red = 'var(--menu-red)';
+
 function menuToggle(subMenuId,itemListId){
-   
 	document.querySelectorAll('.MenuButtons').forEach(e=>e.style.backgroundColor = 'black')
 	document.querySelectorAll('.MenuItems').forEach(e=>e.style.display = 'none')
 
-	document.getElementById(subMenuId).style.backgroundColor = 'red' 
+	document.getElementById(subMenuId).style.backgroundColor = red 
 	document.getElementById(itemListId).style.display = 'block'
 }
+
